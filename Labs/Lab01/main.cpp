0f0ba26140e838df1c500ae58455f10276dfd5b0
Rafael Palomar <rafael.palomar@ntnu.no>
2022-04-27 07:34:36 +0200

    Update Lab01

--- Labs/Lab01/main.cpp
+++ Labs/Lab01/main.cpp
@@ -1,10 +1,38 @@
-#include <iostream>
+
+// TCLAP includes
 #include <tclap/CmdLine.h>
 
-using namespace std;
+//STD includes
+#include <cstdlib>
+#include <string>
 
 int main(int argc, char *argv[]) {
 
+  bool console = false;
 
-  return 0;
+  try {
+
+    TCLAP::CmdLine cmd("Hello OpenGL");
+    TCLAP::SwitchArg consoleSwitch("c", "console", "Console mode", false);
+    cmd.add(consoleSwitch);
+    cmd.parse(argc, argv);
+
+    console = consoleSwitch.getValue();
+
+  } catch (TCLAP::ArgException &e) {
+    std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+
+  if(console)
+  {
+    std::cout << "Hello OpenGL: Console" << std::endl;
+  }
+  else
+  {
+    std::cout << "Hello OpenGL" << std::endl;
+  }
+
+  return EXIT_SUCCESS;
 }
