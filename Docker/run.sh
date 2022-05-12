6f14ebf6173284cf03b2871647606061e12acf95
Benjamin Letnes Bjerken <benjabj@stud.ntnu.no>
2022-05-12 16:47:36 +0200

    Reformat run.sh

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+
+# We build from the parent dir in order to copy it
 cd ..
+
 # Remove the old container
 docker rm -f template
+
 # Build container
 docker build --build-arg cores=1 -f Docker/Dockerfile -t template .
+
 # Start container
 docker run -it -d --name template template /bin/sh
